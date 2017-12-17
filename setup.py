18be1a5e9d24135b75417f5be01f7b378f5cdc97
Luke Gessler <lukegessler@gmail.com>
2017-12-16 22:08:50 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from f3.version import Version
+
+
+setuptools.setup(name='f3',
+                 version=Version('0.0.1').number,
+                 description='Command line tool for extracting word Frequencies From Files',
+                 long_description=open('README.md').read().strip(),
+                 author='Luke Gessler',
+                 author_email='lukegessler@gmail.com',
+                 url='https://github.com/lgessler/f3',
+                 py_modules=['f3'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='word frequency',
+                 classifiers=[])
