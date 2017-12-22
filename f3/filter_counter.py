bc0ccbea0d4b2e02a97e46ea635e90c8355473d9
Luke Gessler <ldg3fa@virginia.edu>
2017-12-21 23:42:25 -0600

    dont use language models

--- f3/filter_counter.py
+++ f3/filter_counter.py
@@ -1,6 +1,12 @@
 from langid.langid import LanguageIdentifier, model
 from collections import defaultdict
+from bs4 import BeautifulSoup
+import enchant
 import re
+
+
+def attempt_parse_html(s):
+    return BeautifulSoup(s, "html.parser")
 
 
 class FilterCounter(object):
@@ -28,16 +34,40 @@ class FilterCounter(object):
 
         freqs = defaultdict(int)
         filtered_freqs = defaultdict(int)
-        pattern = r'\w+'
+        pattern = r'[^\W\d_]+'
+        # count = 0
         for line in lines:
-            lang, _ = identifier.classify(line)
+            # lang, prob = identifier.classify(line)
+            # if not (lang == 'sw' and prob > .99999):
+            #    count += 1
+                # print()
+                # print(line)
+                # print(lang, prob)
+            soup = attempt_parse_html(line)
+            if bool(soup.find()):
+                print("Looks like HTML: \n\t", line)
+                line = soup.text
+                print("Interpreting as this instead: \n\t", line, "\n")
 
             for word in re.findall(pattern, line):
                 if ignore_case:
                     word = word.lower()
-                if lang == self.target_lang:
+                # if lang == self.target_lang:
+                if True:
                     freqs[word] += 1
                 else:
                     filtered_freqs[word] += 1
 
+        en_dict = enchant.Dict("en_US")
+        print("Found {0} words, {1} of which were filtered out"
+              .format(len(freqs),
+                      len([x for x, _ in freqs.items()
+                           if en_dict.check(x)])))
+        for word, freq in \
+                sorted(freqs.items(), reverse=True, key=lambda x: x[1]):
+            if en_dict.check(word):
+                filtered_freqs[word] += freqs[word]
+                freqs.pop(word)
+
         return freqs, filtered_freqs
+
